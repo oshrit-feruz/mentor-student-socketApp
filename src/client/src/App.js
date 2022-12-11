6b269e5daa79bb90d6a47bff4a1716ffd80cbf89
oshrit <oshrit13200@gmail.com>
2022-12-11 14:20:47 +0200

    create table ans insert code block data

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import LogIn from "./routes/logIn";
 
 function App() {
   return (
