a30461c865524c974d545b18c081e7f3f88c2356
oshrit <oshrit13200@gmail.com>
2022-12-11 12:07:16 +0200

    starter project

---
+++ src/client/src/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LogIn from "./routes/logIn";
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
